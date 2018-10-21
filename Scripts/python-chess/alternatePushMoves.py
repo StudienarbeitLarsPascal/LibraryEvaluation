53b856836ed47e1fa95736373d63b7f9b21559e4
PascalSchroederDE <s161904@student.dhbw-mannheim.de>
2018-10-21 02:08:16 +0200

    added alternate User AI Input and push moves tests

---
+++ Scripts/python-chess/alternatePushMoves.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 22:38:23 2018
+
+@author: Schroeder
+"""
+import chess
+import chess.svg
+import random
+board = chess.Board()
+
+# print user possible moves and read in chosen move
+def getUserMove():
+    print("Possible Moves: ")
+    print(getLegalMovesUCI())
+    
+    print("Enter your move:")
+    move = input()
+        
+    return chess.Move.from_uci(move)
+
+# get random move out of legal moves list
+def getRandomMove():
+    return random.choice(list(board.legal_moves))
+
+def getLegalMovesUCI():
+    return list(map(board.uci, board.legal_moves))
+
+# while game is not over alternate user & ai input
+while (not board.is_game_over()):
+    if board.turn:
+        board.push(getUserMove())
+        print("Your Move: ")
+    else:
+        board.push(getRandomMove())
+        print("AIs Move:")
+    
+    print("---------------")
+    print(board)
+    print("---------------")
+    print()
